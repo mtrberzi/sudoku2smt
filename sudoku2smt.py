5b25b29fb94b27714642d1cfa340dcd5a930bbd6
Murphy Berzish <murphy.berzish@gmail.com>
2015-01-20 23:23:45 -0500

    constrain columns and subgrids; full solver

--- sudoku2smt.py
+++ sudoku2smt.py
@@ -59,6 +59,36 @@ def declare_entry(row,col):
     else:
         print("(assert (= %s %s))" % (e, initial_entry))
 
+def constrain_row(row):
+    for i in range(9):
+        for j in range(i+1, 9):
+            e1 = entry(row, i)
+            e2 = entry(row, j)
+            print("(assert (not (= %s %s)))" % (e1, e2))
+
+def constrain_column(col):
+    for i in range(9):
+        for j in range(i+1, 9):
+            e1 = entry(i, col)
+            e2 = entry(j, col)
+            print("(assert (not (= %s %s)))" % (e1, e2))
+
+def constrain_subgrid(x, y):
+    xmin = 3*x
+    xmax = 3*x + 2
+    ymin = 3*y
+    ymax = 3*y + 2
+    for i1 in range(xmin, xmax+1):
+        for j1 in range(ymin, ymax+1):
+            for i2 in range(xmin, xmax+1):
+                for j2 in range(ymin, ymax+1):
+                    if i1 != i2 or j1 != j2:
+                       e1 = entry(i1, j1)
+                       e2 = entry(i2, j2)
+                       print("(assert (not (= %s %s)))" % (e1, e2))
+    
+## Entry point.
+
 # Read 9 lines of input
 for i in range(9):
     line = sys.stdin.readline()
@@ -70,6 +100,19 @@ for row in range(9):
     for col in range(9):
         declare_entry(row, col)
 
+# Constrain all rows
+for row in range(9):
+    constrain_row(row)
+
+# Constrain all columns
+for col in range(9):
+    constrain_column(col)
+
+# Constrain all 3x3 subgrids
+for x in range(3):
+    for y in range(3):
+        constrain_subgrid(x, y)
+        
 # finally
 print("(check-sat)")
 print("(get-model)")
